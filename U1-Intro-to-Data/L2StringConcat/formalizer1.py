af8597f4153eb5d58ccda94181e25fcfd3c1a619
Emily Pensinger <emily.pensinger@gmail.com>
2021-01-06 14:19:36 -0500

    Create formalizer1.py

---
+++ U1-Intro-to-Data/L2StringConcat/formalizer1.py
@@ -0,0 +1,26 @@
+# This program needs to spit out the formal version of a person's name. 
+# Under each person's name, print the correct title - Mr or Ms along with their name.  
+# For example, Joe Smith, male would print out Mr. Smith
+# I'm asking you to use concatenation to combine the title and the last name, even though you could do it another way.
+
+#Peter Parker, male
+
+#Bruce Wayne, male
+
+#Bruce Banner, male
+
+#Clark Kent, male
+
+#Natasha Romanoff, female
+
+#Peter Quill, male
+
+#T'Challa, male
+
+#Steve Rogers, male
+
+#Selina Kyle, female
+
+#Barbara Gordon, female
+
+#Ororo Monroe, female
