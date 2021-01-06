f4183595620d9f5545c155315b134127b7da3463
Emily Pensinger <emily.pensinger@gmail.com>
2021-01-06 14:35:47 -0500

    Create myDigits.py

---
+++ U1-Intro-to-Data/L2StringConcat/myDigits.py
@@ -0,0 +1,7 @@
+# When dialing from overseas to the United States, you have to add stuff to the phone number to get it to go through.
+# In Japan you have to dial 010 to get out of the country, then 1, then the area code and number.
+# So the number 423-264-3445 would be 010-1-423-264-3445
+
+# Use concatenation to combine the digits to dail out of Japan with your phone number and print it out.
+
+
