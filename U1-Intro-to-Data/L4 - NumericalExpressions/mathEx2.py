0cc762a226719a80c42e7c0509621791462faa61
Emily Pensinger <emily.pensinger@gmail.com>
2021-01-07 10:38:02 -0500

    Create mathEx2.py

---
+++ U1-Intro-to-Data/L4 - NumericalExpressions/mathEx2.py
@@ -0,0 +1,28 @@
+print("John and Amy have 48 and 51 quarters, respectively. \
+  If they combine all of their quarters and change as much as possible into dollar bills, \
+  how many quarters will be left?")
+#Type your mathematical expression here after the equals sign here.
+p1 = 0
+print(p1)
+
+print("A baker has 937 oz of flour.  \
+  If each cake requires 16 ounces of flour, \
+  how many cakes can they make?")
+#Type your mathematical expression here after the equals sign here.
+p2 = 0
+print(p2)
+
+print("Find the area of a circle with a radius of 3 (area equals 3.14 times r squared)")
+#Type your mathematical expression here after the equals sign here.
+p3 = 0
+print(p3)
+
+print("Find the square root of 112.")
+#Type your mathematical expression here after the equals sign here.
+p4 = 0
+print(p4)
+
+print("What do you get if you multiply 16 times 16 five times?")
+#Type your mathematical expression here after the equals sign here.
+p5 = 0
+print(p5)
