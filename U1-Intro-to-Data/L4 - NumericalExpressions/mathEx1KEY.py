787a1f629bfbd7c5cde64c6b1cd27043ded063d1
Emily Pensinger <emily.pensinger@gmail.com>
2021-01-07 10:26:52 -0500

    Create mathEx1KEY.py

---
+++ U1-Intro-to-Data/L4 - NumericalExpressions/mathEx1KEY.py
@@ -0,0 +1,24 @@
+print("Find the surface area (length times width) of a wall that is 8 feet tall and 10 feet long.")
+#Type your mathematical expression here after the equals sign here.
+p1 = 8 * 10
+print(p1)
+
+print("Find the circumference of a flower pot that has a diameter of 6  \
+  (circumference is 3.14 times the diameter)")
+#Type your mathematical expression here after the equals sign here.
+p2 = 3.14 * 6
+print(p2)
+
+
+print("After paying seven dollars for the pie, Melanie has 71 dollars left. \
+  How much money did she have before buying the pie ?")
+#Type your mathematical expression here after the equals sign here.
+p3 = 71 + 7
+print(p3)
+
+print(" Alyssa is baking a cake. The recipe calls for 9 cups of flour.\
+  She already put in 4 cups. How many more cups does she need to add ?")
+#Type your mathematical expression here after the equals sign here.
+p4 = 9 - 4
+print(p4)
+
