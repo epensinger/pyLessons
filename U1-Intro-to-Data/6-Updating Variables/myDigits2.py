7607118639714c402873846a641ef337cc32481e
Emily Pensinger <emily.pensinger@gmail.com>
2021-01-11 12:09:01 -0500

    Create myDigits2.py

---
+++ U1-Intro-to-Data/6-Updating Variables/myDigits2.py
@@ -0,0 +1,9 @@
+#Let's apply what we have learned about variables to our digits program from the string concatenation lesson.
+
+
+# When dialing from overseas to the United States, you have to add stuff to the phone number to get it to go through.
+# In Japan you have to dial 010 to get out of the country, then 1, then the area code and number.
+# So the number 423-264-3445 would be 010-1-423-264-3445
+
+# This time store 010-1- in a variable and use concatenation to add it to a string which is a phone number.
+# Store the result of your concatenation in a NEW variable called japan_number
