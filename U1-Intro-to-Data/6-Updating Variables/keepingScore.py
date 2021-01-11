a70331114532b22b823595168f17b5e517c93b8f
Emily Pensinger <emily.pensinger@gmail.com>
2021-01-11 12:05:44 -0500

    Create keepingScore.py

---
+++ U1-Intro-to-Data/6-Updating Variables/keepingScore.py
@@ -0,0 +1,30 @@
+# Write a program that keeps score (in a super limited way)
+
+
+#Start by creating a variable called score and assigning it a value of 0 on the next line
+
+
+print("Press any key to win!")
+input()
+
+#Update the score by adding five to it on line 11:
+
+
+print(f"Your score is now {score}")
+
+print("Press any key to win!")
+input()
+
+#Update the score by adding ten to it on line 19:
+
+
+print(f"Your score is now {score}")
+
+print("Press any key to win!")
+input()
+
+#Update the score by adding 3 to it on line 27:
+
+
+print(f"Your score is now {score}")
+print("You won the silly little game!")
