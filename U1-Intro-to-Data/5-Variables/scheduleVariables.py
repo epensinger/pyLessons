590c398fd9bafa30c809a5630d7caa3a2fd3e8d9
Emily Pensinger <emily.pensinger@gmail.com>
2021-01-13 08:35:10 -0500

    Create scheduleVariables.py

---
+++ U1-Intro-to-Data/5-Variables/scheduleVariables.py
@@ -0,0 +1,4 @@
+# Let's make your schedule as variables
+# Create a variable for each period of the day - use period1, period2 and so on for the variable name.
+# In each variable, store the name of the class as a string.
+# That's all, the goal here is that you get the syntax for variable assignment.
