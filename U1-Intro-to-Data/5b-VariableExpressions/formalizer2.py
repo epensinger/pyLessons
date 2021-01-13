75cb1d6f88a7f6167b596a0cb421bcf5828b145a
Emily Pensinger <emily.pensinger@gmail.com>
2021-01-13 10:01:13 -0500

    Create formalizer2.py

---
+++ U1-Intro-to-Data/5b-VariableExpressions/formalizer2.py
@@ -0,0 +1,29 @@
+#In this assignment we will use variables to increase the efficiency of our formalizer program.
+
+# This program needs to spit out the formal version of a person's name. 
+# Under each person's name, print the correct title - Mr or Ms along with their name.  
+# For example, Joe Smith, male would print out Mr. Smith
+# This is the new part:  store Mr.  and Ms.  in variables and use the variable in your expression.
+# You may copy as much as you like from the previous file, but you must use a variable for Mr. and Ms. 
+
+#Peter Parker, male
+
+#Bruce Wayne, male
+
+#Bruce Banner, male
+
+#Clark Kent, male
+
+#Natasha Romanoff, female
+
+#Peter Quill, male
+
+#T'Challa, male
+
+#Steve Rogers, male
+
+#Selina Kyle, female
+
+#Barbara Gordon, female
+
+#Ororo Monroe, female
