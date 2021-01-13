70f18fd1bc84c2f838dcf84fe8284e2bf5fadcd0
Emily Pensinger <emily.pensinger@gmail.com>
2021-01-13 08:31:20 -0500

    Create lottery.py

---
+++ U1-Intro-to-Data/5b-VariableExpressions/lottery.py
@@ -0,0 +1,9 @@
+#WhooHOOO!   You just won the lottery, after taxes, your winnings are the amount stored in the variable cash below.  
+#Being the wise person that you are, you want to divide your money into 5 equal buckets - one to invest, one to give away to charity, one for each of your parents, and one for you.  
+#Use the variable cash in a math problem to divide your winnings 5 ways.  
+
+
+cash = 19867324678987.99  # DON'T CHANGE THE CASH VARIABLE
+
+# ADD YOUR CODE BELOW:
+
