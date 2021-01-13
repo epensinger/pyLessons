59c6c4db35952243388b2a70f1c23b65116b1eb1
Emily Pensinger <emily.pensinger@gmail.com>
2021-01-13 08:58:04 -0500

    Create pointValues.py

---
+++ U1-Intro-to-Data/5-Variables/pointValues.py
@@ -0,0 +1,4 @@
+# Let's pretend we've got a game where you collect gems and sell them for coins in the game.
+# Create a varaible for several gems - ruby, saphire, diamond, opal, amber.
+# In the variable, store a value between 10 and 100 that represents the amount of coins you will get for that gem.
+
