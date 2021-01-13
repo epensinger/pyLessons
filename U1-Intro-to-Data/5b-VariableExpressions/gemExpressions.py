c16a0c87afa434f8f8efd4dc99f2c86eaf740f0f
Emily Pensinger <emily.pensinger@gmail.com>
2021-01-13 11:50:57 -0500

    Create gemExpressions.py

---
+++ U1-Intro-to-Data/5b-VariableExpressions/gemExpressions.py
@@ -0,0 +1,18 @@
+# We're going to add to the program in which we made variables to store the values of gems.
+
+# In that program your assignment was to do the following:
+# Create a varaible for several gems - ruby, saphire, diamond, opal, amber.
+# In the variable, store a value between 10 and 100 that represents the amount of coins you will get for that gem.
+# You may copy and paste that here or redo it.
+
+# AFTER you have added the variables, we are going to use them to find the value of purses that contain certain gems.
+
+# red_purse contains a ruby, a saphire, and a diamond.  Write an expression using those variables to store the value in red_purse.
+
+
+
+# blue_purse contains a saphire, an opal, amber, and a diamond.  Write an expression using those variables to store the value in blue_purse.
+
+
+
+# green_purse contains 2 diamonds, a ruby, and a saphire.  Write an expression using those variables to store the value in green_purse.
