f7beaa016a31eabe6dbc442309fc5704a93b41ca
Emily Pensinger <emily.pensinger@gmail.com>
2021-01-14 21:16:09 -0500

    Create shoppingSim.py

---
+++ U1-Intro-to-Data/6-Updating Variables/shoppingSim.py
@@ -0,0 +1,56 @@
+#Let's build a little store.  Read along and add the code where intsructed.  
+#Feel free to be creative with the text of your messages and what you sell in the store.
+
+#1.  Set up the store.  Start by creating 5 variables to store the cost of different items.  
+# The items can be whatever you want, go with basic food items if you are stuck - apple, carrot, candy_bar, etc.
+
+
+
+
+
+
+
+
+#2. Give the user some Money.  Create a variable called cash and store 100 in it.  
+#This is how much money you will have when the program starts.
+
+
+
+
+
+
+#4.  Greet the User.  Write a print statement to say something like welcome to the store.  
+
+
+
+
+
+#5. Tell the user what is for sale.  Write a print statement for each item in the store.  
+
+
+
+
+#6. Skipping some logic we don't know yet, pretend your user picked one of the items. 
+#Write a print statement to tell the user what they picked and how much it costs.
+
+
+
+
+#7. Update the value of their money, subracting the value of the item they purchased.
+
+
+
+
+
+#8. Tell them how much money they have left.
+
+
+
+
+#9.  They bought something else, tell them what they bought and how much it costs.
+
+
+
+
+#10. Tell them how much money they have left.
+
