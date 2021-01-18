2d5e3810d1a350326a665ba8e3ba8cb36a161d77
Emily Pensinger <emily.pensinger@gmail.com>
2021-01-18 18:12:15 -0500

    Create caseSensitivity.py

---
+++ U2-Functions/1-StringFunctions/caseSensitivity.py
@@ -0,0 +1,34 @@
+#These lines of code gets a user's First name and last name. 
+#It stores first name and last name in the variables first and last.
+#DON'T TOUCH THEM.
+first = input("What is your first name? ")
+last = input("What is your last name?")
+yell_first = ""
+
+#We will do some conditional tests on the name, so we need to check formatting.
+#Each time you write code, test the user input and make sure it does what you want.
+
+#1. Remove any white space from the left side of first.
+
+
+#2. Remove any white space from the right side of first.
+
+
+#3. Remove any white space on either side of last.
+
+
+#4. Make sure first is all lower case.
+
+
+#5. Make sure last is in title case.
+
+
+#6. Create a variable called yell_first and store the first name in all upper case in it.
+
+ 
+
+
+print("First is " + first)
+print("Last is " + last)
+if yell_first:
+  print("yell_first is " + yell_first)
