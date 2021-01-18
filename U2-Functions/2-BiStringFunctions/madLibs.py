dd4f121dce4f7949b4a45fd5936e5b7d44fb9694
Emily Pensinger <emily.pensinger@gmail.com>
2021-01-18 18:28:12 -0500

    Create madLibs.py

---
+++ U2-Functions/2-BiStringFunctions/madLibs.py
@@ -0,0 +1,14 @@
+#This program works like the game Mad Libs.
+
+#1.  You need to ask the user for noun, verb, adverb, etc.
+#Make sure to set the input equal to variables.
+
+
+#2. Write a story and replace the parts of speach with your variables.
+#Hint: Use a new print statement for each sentence.
+
+
+#3. Try your story out with different words and see how it works.
+
+
+
