29b0e03a7bf121ae1cd7f5e13dc7187205893cbc
Emily Pensinger <emily.pensinger@gmail.com>
2021-01-18 18:30:40 -0500

    Create greetUser.py

---
+++ U2-Functions/2-BiStringFunctions/greetUser.py
@@ -0,0 +1,12 @@
+#Ask the user for their first name.
+
+#Greet them by name.
+
+#Ask the user for their favorite color.
+
+#Tell the user that their favorite color is pretty. (Use the actual color).
+
+#Ask the user for their favorite game.
+
+#Tell them that their favorite game is fun. (Use the actual game).
+
