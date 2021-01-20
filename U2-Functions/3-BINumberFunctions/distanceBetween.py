657724907567a5d5181a9c67e9331f22edec5298
Emily Pensinger <emily.pensinger@gmail.com>
2021-01-20 09:33:28 -0500

    Create distanceBetween.py

---
+++ U2-Functions/3-BINumberFunctions/distanceBetween.py
@@ -0,0 +1,9 @@
+# I have two characters on my platformer
+
+char1_loc = 254
+char2_loc = 478
+
+
+# Write an expression to store the distance between the two points
+# Use absolute value to make sure the distance will be a positive number wherever they go.
+# Print out the result.
