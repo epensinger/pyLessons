ef5e1612f73aaf8a57b6f7b2c0e3f79e229e9712
Emily Pensinger <emily.pensinger@gmail.com>
2021-01-20 09:34:21 -0500

    Create guessing1.py

---
+++ U2-Functions/3-BINumberFunctions/guessing1.py
@@ -0,0 +1,8 @@
+#Ask the user to guess a number between 1 and 10.
+
+#Generate a random number between 1 and 10.
+
+#Find the difference between the user's guess and the number.
+#Use absolute value to make sure the difference is positive.
+
+#Print out a message to tell the user how far they were from the magic number.
