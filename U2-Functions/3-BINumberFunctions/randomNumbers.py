79cf5889592fd5b0f1c211d9fe70444d0780f367
Emily Pensinger <emily.pensinger@gmail.com>
2021-01-20 09:34:02 -0500

    Create randomNumbers.py

---
+++ U2-Functions/3-BINumberFunctions/randomNumbers.py
@@ -0,0 +1,12 @@
+#Start by importing the random module
+
+
+
+#Generate a random integer between 1 and 10 (and print it out)
+
+
+
+#Generate a random integer between 50 and 100 (and print it out)
+
+
+#Generate a random integer between 100 and 200 (and print it out)
