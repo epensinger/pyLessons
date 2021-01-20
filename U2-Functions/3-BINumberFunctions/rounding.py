38a2abecf91595193254e73ae73a326b14047da3
Emily Pensinger <emily.pensinger@gmail.com>
2021-01-20 09:32:33 -0500

    Create rounding.py

---
+++ U2-Functions/3-BINumberFunctions/rounding.py
@@ -0,0 +1,13 @@
+# Round 12345.23552 to the nearest integer then print it out.
+
+
+
+
+# Round 2345 to the nearest hundred then print it out
+
+
+# Round 243.3333 to the nearest hundredth (2 decimal places) then print
+
+
+
+# Round 9872.234758 to the nearest tenth (1 decimal place) then print
