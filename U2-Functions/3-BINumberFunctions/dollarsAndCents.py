fd80dce91dab63039ff9ee005b12f7ded5fd3088
Emily Pensinger <emily.pensinger@gmail.com>
2021-01-20 09:33:03 -0500

    Create dollarsAndCents.py

---
+++ U2-Functions/3-BINumberFunctions/dollarsAndCents.py
@@ -0,0 +1,16 @@
+#Collect user input with the prompt How much money did we make (in digits only)?
+
+
+#Convert the user input into a number
+
+
+#Divide the number by 7
+
+
+#Round the result to two decimal places.
+
+
+#Convert the result back to a string
+
+
+#Print out the new sting in a sentence.
