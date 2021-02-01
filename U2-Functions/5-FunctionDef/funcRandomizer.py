d72f342fb1d5c9db4f39071bb37ba8d06cca0fef
Emily Pensinger <emily.pensinger@gmail.com>
2021-02-01 09:02:40 -0500

    Create funcRandomizer.py

---
+++ U2-Functions/5-FunctionDef/funcRandomizer.py
@@ -0,0 +1,10 @@
+#Randomizer Func Ex
+
+#Write a function random_ones() that prints out  a random number between 1 and 10.
+
+#Write a function random_hundred() that prints out a random number between 1 and 100
+
+#Write a function random_tens() that prints out a random number between 1 and 100 that is evenly divisible by 10.  HInt(you can multiply or use round() to solve this.)
+
+#Call each of the functions
+
