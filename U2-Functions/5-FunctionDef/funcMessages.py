ba0fe63304cfdf3bc74d015f78f47489d4d2992d
Emily Pensinger <emily.pensinger@gmail.com>
2021-02-01 09:02:05 -0500

    Create funcMessages.py

---
+++ U2-Functions/5-FunctionDef/funcMessages.py
@@ -0,0 +1,11 @@
+#Messages Func Ex.
+
+#Write a function prayer() that prints out the first line of the lords prayer.
+
+#Write a function morning() that prints a good morning message.
+
+#Write a function afternoon() that prints a good afternoon message.
+
+#Write a function evening() that prints a good evening message.
+
+#Call each of the functions.
