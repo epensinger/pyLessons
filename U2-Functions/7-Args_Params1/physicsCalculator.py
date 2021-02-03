092083cb26b987d43b1cbf9614d8cdb3aecf0169
Emily Pensinger <emily.pensinger@gmail.com>
2021-02-03 09:27:59 -0500

    Create physicsCalculator.py

---
+++ U2-Functions/7-Args_Params1/physicsCalculator.py
@@ -0,0 +1,15 @@
+# The formula for calculating speed is speed equals distance divided by time.
+# Write a function speed() to calculate the speed of a given distance and time.
+
+
+
+# The formula for calculating distance is speed multiplied by time.
+# Write a function distance() to calculate the distance of a given speed and time.
+
+
+
+# The formula for calculating time is distance divided by speed.
+# Write a function time() to calculate the time of a given distance and speed.
+
+
+
