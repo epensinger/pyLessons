d93a326de59833002dcdf1b8082c9f94459fd6e5
Emily Pensinger <emily.pensinger@gmail.com>
2021-02-03 09:34:42 -0500

    Create yourOwnCalculator.py

---
+++ U2-Functions/7-Args_Params1/yourOwnCalculator.py
@@ -0,0 +1,3 @@
+# Write a comment to explain how your calculator is helpful.
+# Include at least 3 functions that can be called to perform calculations.
+# In each function use a comment to explain what the function takes and what it returns.
