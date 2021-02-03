72dcebd7de8a2fd581e448249f387dc379ae60af
Emily Pensinger <emily.pensinger@gmail.com>
2021-02-03 10:35:01 -0500

    Create updateScore.py

---
+++ U2-Functions/7-Args_Params1/updateScore.py
@@ -0,0 +1,5 @@
+# Write a function score() that:
+
+# Takes two parameters - one for the current score and one for the point value you want to add to it.
+
+# Adds the new points to the score and returns the new score.
