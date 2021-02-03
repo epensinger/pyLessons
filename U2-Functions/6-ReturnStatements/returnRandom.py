71890ac6648d5932add2bfd6f9a256066fbff4ef
Emily Pensinger <emily.pensinger@gmail.com>
2021-02-03 10:29:24 -0500

    Create returnRandom.py

---
+++ U2-Functions/6-ReturnStatements/returnRandom.py
@@ -0,0 +1,3 @@
+# In the last lesson you created a function that printed a random number.
+# Use the same 3 functions here, but this time RETURN the random number instead of printing it.
+# You may copy your work from funcRandomizer and then change each function to return instead of print.
