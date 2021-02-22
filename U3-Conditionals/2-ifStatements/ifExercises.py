69f89f24e53260968e1e2ae2ff40fcf6f7b147bb
Emily Pensinger <emily.pensinger@gmail.com>
2021-02-22 09:22:15 -0500

    Create ifExercises.py

---
+++ U3-Conditionals/2-ifStatements/ifExercises.py
@@ -0,0 +1,44 @@
+ # If exercises
+
+#1.  Create a variable cash and store some money in it.  
+# Print out "You don't have enough money" if the cash is less than 45.
+# Do that in this space right here.
+
+
+
+
+
+#2.  Create a variable name and store a name in it.
+# Print out "That's my name!" if the string in name is the same as your name.
+
+
+
+
+
+
+#3.  Create a variable magic_number and store a number in it.
+# Create a variable guess and store a number in it.
+# Print out "You haven't guessed the magic number" if the guess is not the magic number.
+
+
+
+
+
+
+#4.  Create a variable answer and store an empty string in it ("").
+# Write an if statement that will print "You didn't say anything!" if answer is empty.
+
+
+
+
+#5.  Create a variable lives and store a number in it.
+# Write an if statement that will print "You have run out of lives" if lives gets to 0.
+
+
+
+
+
+
+
+
+
