c4adc1643408fb949061aeb6de8bf6e3d7130042
Emily Pensinger <emily.pensinger@gmail.com>
2022-09-27 10:12:36 -0400

    Create if_statements_assign.py

---
+++ U3-Conditionals/if_statements_assign.py
@@ -0,0 +1,25 @@
+#DON'T FORGET TO CREATE ANY VARIABLES YOU MIGHT NEED
+#1.If the amount stored in the variable cash is enough to buy an item that costs $2.00,
+#Print you can afford the candy!
+
+
+#2.If the guest is younger than 7, print I'm sorry you are too little.
+
+
+#3.If the speed is greater than 80, print you need to slow down.
+
+
+
+#4.  If the grade is greater than or equal to 65, print you passed.
+
+
+
+#5. If the number of students is less than or equal to 24, print this class is not overcroweded.
+
+
+
+#6. If player1's score is greater than player 2's score, print player 1 wins!
+
+
+
+#7.  If the enemy's health is less than or equal to zero, print you have defeated the enemy!
