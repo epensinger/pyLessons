0c353798a67a656e40e64b0e4e69955be95ab209
Emily Pensinger <emily.pensinger@gmail.com>
2022-10-05 09:37:15 -0400

    Create Multi-selectionAssign.py

---
+++ U3-Conditionals/Multi-selectionAssign.py
@@ -0,0 +1,32 @@
+#####THERE ARE FOUR (4) different conditional statements you will need to complete in this assignment.#####
+
+#1.  Write a 2-way selection statement to give your player a realistic message.
+     #Create a variable user_health and a variable enemy_health.  
+     #Store some numbers in your variables.  
+     #If the user_health is greater than the enemey_health print out "You are beating the enemy!"
+     #Otherwise it should print "Fight harder, weakling!"
+
+      
+      
+      
+#2. Write a two-way selection statement to see if the user has enough cash to make a purchase.  
+    #Create a variable cash and store a number in it.  
+    #If the user can purchase something for 10, print that they can.  
+    #If they can't, tell them they need more money.
+    
+    
+    
+#3. Write a multi-selection statement to determine where a student should go based on their grade.
+    #Get user input for the grade as a number (remember int())
+    #if the grade is higher than 93 tell them they can go play, they have earned a break.
+    #if the grade is between 75 and 92, tell them they should go to study hall.
+    #If the grade is between 60 and 75, tell them they should see a tutor.
+    #If the grade is below a 60, tell them they should go to detention.
+    
+#4. Write a multi-selection statement to determine the charge for a user's tickets.
+    #If the age is greater than 65 cost is 3.50
+    #If the age is between 22 and 64 cost is 5.00
+    #If the age is between 6 and 22 cost is 3.50
+    #If the age is less than 6, cost is free.
+    #INSTEAD of PRINTING, this time, your conditional should store the cost in a vairable - cost.
+    #Print cost at the end for testing.
