016dc050719cc187773fd510f4cbf4f437d29774
Emily Pensinger <emily.pensinger@gmail.com>
2022-10-28 09:55:28 -0400

    Create returning_booleans.py

---
+++ U3-Conditionals/returning_booleans.py
@@ -0,0 +1,25 @@
+#1. Write a function called needJacket that takes two parameters - temp and conditions
+      #The temp will be a float (number with a decimal)
+      #The conditions will be a string - fog, rain, sun, clouds, storm, snow
+      #Here's when you need a jacket:
+        #A. when it's colder than 50 degrees regardless of the weather.
+        #B. when it's colder than 60 degrees and fog or clouds
+        #C. when it's any temperature and rain, storm, or snow.
+      #Your function should return true if you need a jacket and false if you don't need a jacket.
+      
+      
+      
+      
+#2. Write a function called big_Enough that takes three integers - length, width, size_needed
+      #The function should return true if the area (length times width) is larger than the size needed.
+      #The function should return false if the area (length times width) is smaller than the size needed.
+    
+    
+    
+#3.  Write a function called will_fit that takes four numbers, the length and width of an object and the length and width of a space
+      #Write a function that checks to see if the object will fit.  
+      #Make sure to check to see if the object will fit if you rotate it.
+      #Return true if it fits, false if it doesn't.
+      
+      
+      
